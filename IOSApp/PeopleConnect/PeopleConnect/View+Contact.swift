d3c25be7eea7cd0e25d8715801638eaa5041dbc7
apple <apple@appledeiMac.local>
2018-12-25 22:54:38 +0800

    postsview code clean

--- IOSApp/PeopleConnect/PeopleConnect/View+Contact.swift
+++ IOSApp/PeopleConnect/PeopleConnect/View+Contact.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class ContactView: UIViewController, PostDataDelegate, UITableViewDataSource, UITableViewDelegate {
+class ContactView: PostsTable, UITableViewDelegate {
     
     @IBOutlet weak var m_name: UILabel!
     @IBOutlet weak var m_profile: UIImageView!
@@ -25,19 +25,13 @@ class ContactView: UIViewController, PostDataDelegate, UITableViewDataSource, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        m_posts.registerClass(PostHeader.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostHeader")
-        m_posts.registerClass(PostFooter.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostFooter")
-        contactPosts.m_delegate = self
+        setTable(m_posts, data: contactPosts, fullView: false)
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         m_name.text = m_contact.name
         m_profile.image = UIImage(data: getContactPhoto(m_contact.user)!)
-    }
-    
-    func PostDataUpdated() {
-        m_posts.reloadData()
     }
     
     func requestNameChanged(sender:UITextField) {
@@ -81,51 +75,6 @@ class ContactView: UIViewController, PostDataDelegate, UITableViewDataSource, UI
     }
     
     @IBAction func RemContact(sender: AnyObject) {
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return contactPosts.m_posts.count
-    }
-    
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let post = contactPosts.m_posts[section]
-        return post.getHeight(m_posts.contentSize.width, fullView: false)
-    }
-    
-    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return PostFooter.Height
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        let post = contactPosts.postAtIdx(indexPath.section)
-        let comment = post.m_comments[indexPath.row]
-        return getTextHeight(comment.getString(), width: m_posts.contentSize.width, font: commentFont)
-    }
-    
-    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostHeader") as! PostHeader
-        let post = contactPosts.m_posts[section]
-        header.m_father = self
-        header.reload(post, width:m_posts.contentSize.width, fullView: false)
-        return header
-    }
-    
-    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footer =  tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostFooter") as! PostFooter
-        footer.reload(m_posts.contentSize.width)
-        return footer
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contactPosts.postAtIdx(section).m_comments.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("CommentCell", forIndexPath: indexPath) as! CommentCell
-        let post = contactPosts.postAtIdx(indexPath.section)
-        let comment = post.m_comments[indexPath.row]
-        cell.m_comment.attributedText = comment.getAttrString()
-        return cell
     }
 }
 
--- IOSApp/PeopleConnect/PeopleConnect/View+Posts.swift
+++ IOSApp/PeopleConnect/PeopleConnect/View+Posts.swift
@@ -12,7 +12,76 @@ class CommentCell: UITableViewCell {
     @IBOutlet weak var m_comment: UILabel!
 }
 
-class PostsView: UIViewController, PostDataDelegate, UITableViewDataSource, UITableViewDelegate {
+class PostsTable: UIViewController, PostDataDelegate, UITableViewDataSource {
+    
+    var m_data: PostData? = nil
+    var m_table: UITableView? = nil
+    var m_fullView: Bool = true
+    
+    func setTable(table:UITableView, data:PostData, fullView: Bool) {
+        m_data = data
+        m_data?.m_delegate = self
+        
+        m_table = table
+        m_table!.registerClass(PostHeader.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostHeader")
+        m_table!.registerClass(PostFooter.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostFooter")
+        m_table!.estimatedRowHeight = 16
+        m_table!.rowHeight = UITableViewAutomaticDimension
+        
+        m_fullView = fullView
+    }
+    
+    func PostDataUpdated() {
+        m_table!.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return m_data!.numOfPosts()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return m_data!.postAtIdx(section).m_comments.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CommentCell", forIndexPath: indexPath) as! CommentCell
+        let post = m_data!.postAtIdx(indexPath.section)
+        let comment = post.m_comments[indexPath.row]
+        cell.m_comment.attributedText = comment.getAttrString()
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostHeader") as! PostHeader
+        let post = m_data!.postAtIdx(section)
+        header.m_father = self
+        header.reload(post, width:m_table!.contentSize.width, fullView: m_fullView)
+        return header
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer =  tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostFooter") as! PostFooter
+        footer.reload(m_table!.contentSize.width)
+        return footer
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let post = m_data!.postAtIdx(indexPath.section)
+        let comment = post.m_comments[indexPath.row]
+        return getTextHeight(comment.getString(), width: m_table!.contentSize.width, font: commentFont)
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let post = m_data!.postAtIdx(section)
+        return post.getHeight(m_table!.contentSize.width, fullView: m_fullView)
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return PostFooter.Height
+    }
+}
+
+class PostsView: PostsTable, UITableViewDelegate {
     
     @IBOutlet weak var m_tabs: UISegmentedControl!
     @IBOutlet weak var m_posts: UITableView!
@@ -22,18 +91,9 @@ class PostsView: UIViewController, PostDataDelegate, UITableViewDataSource, UITa
     
     override func viewDidLoad() {
         //m_posts.registerNib(UINib(nibName: "PostHeader", bundle: nil), forHeaderFooterViewReuseIdentifier: "PostHeader")
-        m_posts.registerClass(PostHeader.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostHeader")
-        m_posts.registerClass(PostFooter.classForCoder(), forHeaderFooterViewReuseIdentifier: "PostFooter")
-
-        friendPosts.m_delegate = self
-        
+        super.viewDidLoad()
+        setTable(m_posts, data: friendPosts, fullView: true)
         httpSyncPost()
-        m_posts.estimatedRowHeight = 16
-        m_posts.rowHeight = UITableViewAutomaticDimension
-    }
-    
-    func PostDataUpdated() {
-        m_posts.reloadData()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -41,51 +101,6 @@ class PostsView: UIViewController, PostDataDelegate, UITableViewDataSource, UITa
             let to = segue.destinationViewController as! ContactView
             to.m_contact = contactsData.m_contacts[ContactView.ContactID]!
         }
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return friendPosts.numOfPosts()
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendPosts.postAtIdx(section).m_comments.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("CommentCell", forIndexPath: indexPath) as! CommentCell
-        let post = friendPosts.postAtIdx(indexPath.section)
-        let comment = post.m_comments[indexPath.row]
-        cell.m_comment.attributedText = comment.getAttrString()
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostHeader") as! PostHeader
-        let post = friendPosts.postAtIdx(section)
-        header.m_father = self
-        header.reload(post, width:m_posts.contentSize.width, fullView: true)
-        return header
-    }
-    
-    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footer =  tableView.dequeueReusableHeaderFooterViewWithIdentifier("PostFooter") as! PostFooter
-        footer.reload(m_posts.contentSize.width)
-        return footer
-    }
-
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        let post = friendPosts.postAtIdx(indexPath.section)
-        let comment = post.m_comments[indexPath.row]
-        return getTextHeight(comment.getString(), width: m_posts.contentSize.width, font: commentFont)
-    }
-    
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let post = friendPosts.postAtIdx(section)
-        return post.getHeight(m_posts.contentSize.width, fullView: true)
-    }
-    
-    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return PostFooter.Height
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
