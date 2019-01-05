1f906d63ed1f4de21b414b4de9cb4a68cd8a9d98
apple <apple@appledeiMac.local>
2019-01-06 00:54:18 +0800

    visible tags UI

--- IOSApp/PeopleConnect/PeopleConnect/View+CreatePost.swift
+++ IOSApp/PeopleConnect/PeopleConnect/View+CreatePost.swift
@@ -9,77 +9,6 @@
 import UIKit
 import Photos
 
-class PostTagCell:UICollectionViewCell {
-    @IBOutlet weak var m_tagName: UILabel!
-}
-
-class PostTags:UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    var m_tags:Array<Tag>?
-    var m_flag:UInt64 = 0
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return m_tags!.count
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PostTagCell", forIndexPath: indexPath) as! PostTagCell
-        cell.m_tagName.text = m_tags![indexPath.row].m_tagName
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
-        let cell = collectionView.cellForItemAtIndexPath(indexPath)
-        cell?.backgroundColor = UIColor.lightGrayColor()
-        m_flag &= ~(m_tags![indexPath.row].m_bit)
-    }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        let cell = collectionView.cellForItemAtIndexPath(indexPath)
-       m_flag |= (m_tags![indexPath.row].m_bit)
-        cell?.backgroundColor = UIColor(red: 0, green: 0.4, blue: 0.8, alpha: 1.0)
-    }
-}
-
-class PostGroupCell:UICollectionViewCell {
-    @IBOutlet weak var m_groupName: UILabel!
-}
-
-class PostGroups:UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    var m_selectGroups = Set<UInt32>()
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return userInfo.groups.count
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PostGroupCell", forIndexPath: indexPath) as! PostGroupCell
-        cell.m_groupName.text = userInfo.groups[indexPath.row].name
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
-        let cell = collectionView.cellForItemAtIndexPath(indexPath)
-        cell?.backgroundColor = UIColor.lightGrayColor()
-        m_selectGroups.remove(userInfo.groups[indexPath.row].id)
-    }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        let cell = collectionView.cellForItemAtIndexPath(indexPath)
-        cell?.backgroundColor = UIColor(red: 0, green: 0.4, blue: 0.8, alpha: 1.0)
-        m_selectGroups.insert(userInfo.groups[indexPath.row].id)
-    }
-}
-
 class CreatePostView:
     UITableViewController,
     UITextFieldDelegate,
@@ -87,13 +16,10 @@ class CreatePostView:
     UpdateLocationDelegate {
     
     @IBOutlet weak var m_desc: UITextField!
-    @IBOutlet weak var m_postTags: UICollectionView!
-    @IBOutlet weak var m_postTagsCell: PostTags!
-    @IBOutlet weak var m_postGroupsCell: PostGroups!
-    @IBOutlet weak var m_postGroups: UICollectionView!
     @IBOutlet weak var m_strangerSee: UISwitch!
     @IBOutlet weak var m_createPostBtn: UIBarButtonItem!
     @IBOutlet weak var m_imgsPreview: ImgPreview!
+    @IBOutlet weak var m_visibleTags: TagsView!
     
     func updateCreateBtn() {
         m_createPostBtn.enabled = (m_imgsPreview.m_picks.count > 0 || m_desc.text?.characters.count > 0)
@@ -117,8 +43,8 @@ class CreatePostView:
     
     @IBAction func CreatePost(sender: AnyObject) {
         let desc = m_desc.text!
-        let flag = m_postTagsCell.m_flag
-        let groups = Array(m_postGroupsCell.m_selectGroups)
+        let flag = m_visibleTags.m_flag
+        let groups = Array<UInt32>()
         let nearby = m_strangerSee.on
         var datas = Array<NSData>()
         for image in m_imgsPreview.m_picks {
@@ -131,31 +57,24 @@ class CreatePostView:
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        m_postTags.allowsSelection = true
-        m_postTags.allowsMultipleSelection = true
-        m_postGroups.allowsSelection = true
-        m_postGroups.allowsMultipleSelection = true
         m_createPostBtn.enabled = false
         m_imgsPreview.m_controller = self
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        m_postTagsCell.m_tags = contactsData.getPostTags()
-        m_postTagsCell.m_flag = 0
-        m_postGroupsCell.m_selectGroups.removeAll()
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
         updateCreateBtn()
     }
     
-    let pickerSpace:CGFloat = 8
+    let space:CGFloat = 8
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         if indexPath.section == 1 && indexPath.row == 0 {
-            let width = tableView.contentSize.width - pickerSpace * 2
-            return (width - PostPreViewGap*2) / 3 + pickerSpace * 2
+            let width = tableView.contentSize.width - space * 2
+            return (width - PostPreViewGap*2) / 3 + space * 2
+        }
+        if indexPath.section == 2 && indexPath.row == 0 {
+            let width = tableView.contentSize.width - space * 2
+            return contactsData.getTagsHeight(width) + space * 2
         }
         return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
     }
@@ -163,11 +82,16 @@ class CreatePostView:
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAtIndexPath: indexPath)
         if indexPath.section == 1 && indexPath.row == 0 {
-            let width = tableView.contentSize.width - pickerSpace * 2
-            m_imgsPreview.frame = CGRectMake(pickerSpace, pickerSpace, width, (width - PostPreViewGap*2) / 3)
+            let width = tableView.contentSize.width - space * 2
+            let height = (width - PostPreViewGap*2) / 3
+            m_imgsPreview.frame = CGRectMake(space, space, width, height)
             m_imgsPreview.reloadEdit()
-            print(m_imgsPreview.frame)
-            print(tableView.contentSize)
+        }
+        if indexPath.section == 2 && indexPath.row == 0 {
+            let width = tableView.contentSize.width - space * 2
+            let height = contactsData.getTagsHeight(width)
+            m_visibleTags.frame = CGRectMake(space, space, width, height)
+            m_visibleTags.loadContactTags(width)
         }
         return cell
     }
--- IOSApp/PeopleConnect/PeopleConnect/View+Tags.swift
+++ IOSApp/PeopleConnect/PeopleConnect/View+Tags.swift
@@ -15,8 +15,15 @@ let TagColors = [
     4:UIColor.yellowColor(),
     5:UIColor.purpleColor()]
 
+let tagEditFont = UIFont.systemFontOfSize(15)
+
 let PostTagHeight:CGFloat = 15
 let PostTagSpace:CGFloat = 15
+
+let EditTagExWidth:CGFloat = 20
+let EditTagHeight:CGFloat = 30
+let EditTagSpace:CGFloat = 10
+let EditTagLineSpace:CGFloat = 20
 
 extension Post {
     func getPermitHeight(width:CGFloat)->CGFloat {
@@ -50,27 +57,122 @@ extension Post {
     }
 }
 
+extension ContactsData {
+    func getTagsHeight(width:CGFloat)->CGFloat {
+        var numLines = 1
+        var lineEnd:CGFloat = 0
+        let tags = contactsData.getPostTags()
+        
+        for tag in tags {
+            let tagWidth = getTextWidth(tag.m_tagName, height: EditTagHeight, font: tagEditFont) + EditTagExWidth
+            
+            if tagWidth + lineEnd > width {
+                numLines++
+                lineEnd = 0
+            }
+            
+            lineEnd += (tagWidth + EditTagSpace)
+        }
+        
+        return CGFloat(numLines) * EditTagHeight + CGFloat(numLines-1) * EditTagLineSpace
+    }
+}
+
+class TagLabel:UILabel {
+    var m_data:UInt64 = 0
+    var m_hilighted = false
+    var m_hilightColor = UIColor.clearColor()
+    var m_father:TagsView? = nil
+    
+    func tap() {
+        m_hilighted = !m_hilighted
+        if m_hilighted {
+            backgroundColor = m_hilightColor
+            m_father?.m_flag |= m_data
+        }
+        else {
+            backgroundColor = UIColor.grayColor()
+            m_father?.m_flag &= ~m_data
+        }
+    }
+}
+
 class TagsView: UIView {
-    let MaxTags = 32
-    var m_tags = Array<UILabel>()
+    let MaxTags = 64
+    var m_tags = Array<TagLabel>()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        initSubViews(false)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubViews(true)
+    }
+    
+    func initSubViews(edit:Bool) {
         for _ in 0...MaxTags-1 {
-            let tag = UILabel(frame: CGRectZero)
-            tag.font = statusFont
+            let tag = TagLabel(frame: CGRectZero)
+            let tap = UITapGestureRecognizer(target: tag, action: Selector("tap"))
+            tag.userInteractionEnabled = true
+            tag.addGestureRecognizer(tap)
+            tag.font = edit ? tagEditFont : statusFont
             tag.layer.cornerRadius = 8
             tag.layer.masksToBounds = true
             tag.hidden = true
             tag.textAlignment = .Center
             tag.textColor = UIColor.whiteColor()
+            tag.m_father = self
             m_tags.append(tag)
             addSubview(tag)
         }
     }
+    
+    var m_curTag = 0
+    var m_numLines = 0
+    var m_lineEnd:CGFloat = 0
+    var m_width:CGFloat = 0
+    
+    var m_flag:UInt64 = 0
+    
+    func pushTag(tag:Tag, font:UIFont) {
+        let colorTagID = (tag.m_father != nil ? tag.m_father?.m_tagID : tag.m_tagID)
+        let color = TagColors[Int(colorTagID!)]
+        let tagWidth = getTextWidth(tag.m_tagName, height: EditTagHeight, font: font) + EditTagExWidth
+        let tagLabel = m_tags[m_curTag]
+        
+        if tagWidth + m_lineEnd > m_width {
+            m_numLines++
+            m_lineEnd = 0
+        }
+        
+        tagLabel.m_data = tag.m_bit
+        tagLabel.hidden = false
+        tagLabel.text = tag.m_tagName
+        tagLabel.backgroundColor = UIColor.grayColor()
+        tagLabel.m_hilightColor = color!
+        tagLabel.frame = CGRectMake(m_lineEnd, CGFloat(m_numLines)*(EditTagHeight+EditTagLineSpace), tagWidth, EditTagHeight)
+        
+        m_lineEnd += (tagWidth + EditTagSpace)
+        m_curTag++
+    }
+    
+    func loadContactTags(width:CGFloat) {
+        for tag in m_tags {
+            tag.hidden = true
+        }
+        
+        m_curTag = 0
+        m_numLines = 0
+        m_lineEnd = 0
+        m_width = width
 
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        let tags = contactsData.getPostTags()
+        
+        for (_, tag) in tags.enumerate() {
+            pushTag(tag, font: tagEditFont)
+        }
     }
     
     func load(post:Post, width:CGFloat) {
