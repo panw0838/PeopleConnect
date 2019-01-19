290f9dd5ba4f9aed9e6a4e4ad4262c3d72b4702d
apple <apple@appledeiMac.local>
2019-01-19 18:49:25 +0800

    count button and log view update

--- IOSApp/PeopleConnect/PeopleConnect/View+Login.swift
+++ IOSApp/PeopleConnect/PeopleConnect/View+Login.swift
@@ -11,14 +11,61 @@ import PhotosUI
 
 var countryDict:Dictionary<Int, CountryInfo>? = nil
 
+class CountButton:UIButton {
+    var m_counts = 60
+    
+    func startCount() {
+        enabled = false
+        m_counts = 60
+        //获取全局队列
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        //创建一个定时器，并将定时器的任务交给全局队列执行(并行，不会造成主线程阻塞)
+        let timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+        // 设置触发的间隔时间
+        dispatch_source_set_timer(timer, dispatch_walltime(nil, 0), NSEC_PER_SEC, 0);
+        //1.0 * NSEC_PER_SEC  代表设置定时器触发的时间间隔为1s
+        //0 * NSEC_PER_SEC    代表时间允许的误差是 0s
+        
+        //block内部 如果对当前对象的强引用属性修改 应该使用__weak typeof(self)weakSelf 修饰  避免循环调用
+        //weak var weakSelf = self
+        //设置定时器的触发事件
+        dispatch_source_set_event_handler(timer, {
+            //1. 每调用一次 时间-1s
+            self.m_counts--
+            if (self.m_counts <= 0) {
+                //关闭定时器
+                dispatch_source_cancel(timer)
+                
+                //在主线程中对button进行修改操作
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.setTitle("获取验证码", forState: .Normal)
+                    self.enabled = true
+                });
+            }
+            else {
+                //处于正在倒计时，在主线程中刷新button上的title，时间-1秒
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.setTitle(String(self.m_counts), forState: .Normal)
+                });
+            }
+        });
+        
+        dispatch_resume(timer);
+    }
+}
+
 class BaseLogRegView: UITableViewController {
     @IBOutlet weak var m_countryBtn: UIButton!
     @IBOutlet weak var m_cellBtn: UIButton!
-    @IBOutlet weak var m_passBtn: UIButton!
+    @IBOutlet weak var m_getCodeBtn: CountButton!
+
+    var m_basePassBtn: UIButton?
+    var m_baseCodeBtn: UIButton?
 
     var m_countryCode:Int = 86
     var m_cellNumber:String = ""
     var m_password:String = ""
+    var m_code:String = ""
 
     func countryCodeChanged(sender:UITextField) {
         let alert = self.presentedViewController as! UIAlertController
@@ -92,9 +139,9 @@ class BaseLogRegView: UITableViewController {
         })
         alert.addTextFieldWithConfigurationHandler {
             (textField: UITextField!) -> Void in
-            textField.placeholder = ""
-            textField.keyboardType = .NumberPad
-            textField.addTarget(self, action: Selector("cellNumberChanged:"), forControlEvents: .EditingChanged)
+                textField.placeholder = ""
+                textField.keyboardType = .NumberPad
+                textField.addTarget(self, action: Selector("cellNumberChanged:"), forControlEvents: .EditingChanged)
         }
         okAction.enabled = false
         alert.addAction(noAction)
@@ -102,16 +149,75 @@ class BaseLogRegView: UITableViewController {
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
+    func passwordChanged(sender:UITextField) {
+        let alert = self.presentedViewController as! UIAlertController
+        let okAction:UIAlertAction = alert.actions.last!
+        m_password = sender.text!
+        let inputSize = m_password.characters.count
+        okAction.enabled = (inputSize >= 8 && inputSize <= 16)
+    }
+    
+    @IBAction func inputPassword(sender:AnyObject) {
+        let alert = UIAlertController(title: "请输入密码", message: "", preferredStyle: .Alert)
+        let noAction = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
+        let okAction = UIAlertAction(title: "确定", style: .Default,
+            handler: { action in
+                self.m_password = (alert.textFields?.first?.text)!
+                self.m_basePassBtn?.setTitle(self.m_password, forState: .Normal)
+                self.updateNextButton()
+        })
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+            textField.placeholder = "请输入8到16位密码"
+            textField.keyboardType = .ASCIICapable
+            textField.addTarget(self, action: Selector("passwordChanged:"), forControlEvents: .EditingChanged)
+        }
+        okAction.enabled = false
+        alert.addAction(noAction)
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func getVerifyCode(sender: AnyObject) {
+        m_getCodeBtn.startCount()
+    }
+
+    func codeChanged(sender:UITextField) {
+        let alert = self.presentedViewController as! UIAlertController
+        let okAction:UIAlertAction = alert.actions.last!
+        m_code = sender.text!
+        okAction.enabled = (m_code.characters.count == 4)
+    }
+    
+    @IBAction func inputCode(sender:AnyObject) {
+        let alert = UIAlertController(title: "请输入验证码", message: "", preferredStyle: .Alert)
+        let noAction = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
+        let okAction = UIAlertAction(title: "确定", style: .Default,
+            handler: { action in
+                self.m_baseCodeBtn?.setTitle(self.m_code, forState: .Normal)
+                self.updateNextButton()
+        })
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+            textField.placeholder = "请输入4位验证码"
+            textField.keyboardType = .NumberPad
+            textField.addTarget(self, action: Selector("codeChanged:"), forControlEvents: .EditingChanged)
+        }
+        okAction.enabled = false
+        alert.addAction(noAction)
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
     func updateNextButton() {
     }
 }
 
 class LogView: BaseLogRegView {
     @IBOutlet weak var m_logSwitchBtn: UIButton!
-    @IBOutlet weak var m_getCodeBtn: UIButton!
     @IBOutlet weak var m_passLabel: UILabel!
     @IBOutlet weak var m_photo: UIImageView!
-    
+    @IBOutlet weak var m_passCodeBtn: UIButton!
     @IBOutlet weak var m_logBtn: UIButton!
     
     var m_usePassword = true
@@ -122,7 +228,7 @@ class LogView: BaseLogRegView {
         super.viewDidLoad()
         m_cellBtn.layer.cornerRadius = 10
         m_countryBtn.layer.cornerRadius = 10
-        m_passBtn.layer.cornerRadius = 10
+        m_passCodeBtn.layer.cornerRadius = 10
         m_photo.layer.cornerRadius = 10
         m_photo.layer.masksToBounds = true
         m_countryBtn.setTitle(getCountryCodeString(), forState: .Normal)
@@ -130,6 +236,11 @@ class LogView: BaseLogRegView {
         m_logBtn.enabled = false
         m_enableColor = m_logBtn.backgroundColor
         m_logBtn.backgroundColor = UIColor.grayColor()
+        
+        m_basePassBtn = m_passCodeBtn
+        m_baseCodeBtn = nil
+        
+        m_passCodeBtn.addTarget(self, action: Selector("inputPassword:"), forControlEvents: .TouchDown)
         
         if userData.getCurUser() {
             let photo = getContactPhoto(userInfo.userID)
@@ -143,56 +254,31 @@ class LogView: BaseLogRegView {
         }
     }
     
-    func passwordChanged(sender:UITextField) {
-        let alert = self.presentedViewController as! UIAlertController
-        let okAction:UIAlertAction = alert.actions.last!
-        let input:String = (sender.text)!
-        let inputSize = input.characters.count
-        
-        if m_usePassword {
-            okAction.enabled = (inputSize >= 8 && inputSize <= 16)
-        }
-        else {
-            okAction.enabled = (inputSize == 4)
-        }
-    }
-    
-    @IBAction func inputPassword(sender:AnyObject) {
-        let title = m_usePassword ? "请输入密码" : "请输入验证码"
-        let alert = UIAlertController(title: title, message: "", preferredStyle: .Alert)
-        let noAction = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
-        let okAction = UIAlertAction(title: "确定", style: .Default,
-            handler: { action in
-                let input = (alert.textFields?.first?.text)!
-                self.m_password = input
-                self.m_passBtn.setTitle((self.m_usePassword ? "********" : input), forState: .Normal)
-                self.updateNextButton()
-        })
-        alert.addTextFieldWithConfigurationHandler {
-            (textField: UITextField!) -> Void in
-            textField.placeholder = "请输入" + (self.m_usePassword ? "8到16位密码" : "4位验证码")
-            textField.keyboardType = (self.m_usePassword ? .ASCIICapable : .NumberPad)
-            textField.addTarget(self, action: Selector("passwordChanged:"), forControlEvents: .EditingChanged)
-        }
-        okAction.enabled = false
-        alert.addAction(noAction)
-        alert.addAction(okAction)
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
     @IBAction func changeLogMethod(sender: AnyObject) {
         m_usePassword = !m_usePassword
         if m_usePassword {
-            m_passBtn.setTitle("请输入密码", forState: .Normal)
+            m_basePassBtn = m_passCodeBtn
+            m_baseCodeBtn = nil
+            
+            m_passCodeBtn.removeTarget(self, action: Selector("inputCode:"), forControlEvents: .TouchDown)
+            m_passCodeBtn.addTarget(self, action: Selector("inputPassword:"), forControlEvents: .TouchDown)
+            
+            m_passCodeBtn.setTitle("请输入密码", forState: .Normal)
             m_getCodeBtn.hidden = true
-            m_logSwitchBtn.setTitle("验证码登陆", forState: .Normal)
-            m_passLabel.text = "验证码"
+            m_logSwitchBtn.setTitle("忘记密码", forState: .Normal)
+            m_passLabel.text = "密码"
         }
         else {
-            m_passBtn.setTitle("请输入验证码", forState: .Normal)
+            m_basePassBtn = nil
+            m_baseCodeBtn = m_passCodeBtn
+            
+            m_passCodeBtn.removeTarget(self, action: Selector("inputPassword:"), forControlEvents: .TouchDown)
+            m_passCodeBtn.addTarget(self, action: Selector("inputCode:"), forControlEvents: .TouchDown)
+
+            m_passCodeBtn.setTitle("请输入验证码", forState: .Normal)
             m_getCodeBtn.hidden = false
-            m_logSwitchBtn.setTitle("密码登陆", forState: .Normal)
-            m_passLabel.text = "密码"
+            m_logSwitchBtn.setTitle("返回密码登陆", forState: .Normal)
+            m_passLabel.text = "验证码"
         }
     }
 
@@ -226,6 +312,8 @@ class RegView: BaseLogRegView, PhotoClipperDelegate, UINavigationControllerDeleg
     @IBOutlet weak var m_nickNameBtn: UIButton!
     @IBOutlet weak var m_regBtn: UIButton!
     @IBOutlet weak var m_photoBtn: UIButton!
+    @IBOutlet weak var m_passBtn: UIButton!
+    @IBOutlet weak var m_codeBtn: UIButton!
     
     var m_photo:NSData? = nil
     var m_nickName:String = ""
@@ -251,36 +339,6 @@ class RegView: BaseLogRegView, PhotoClipperDelegate, UINavigationControllerDeleg
         let navi = UINavigationController(rootViewController: m_picker)
         navi.delegate = self
         self.presentViewController(navi, animated: true, completion: nil)
-    }
-    
-    func passwordChanged(sender:UITextField) {
-        let alert = self.presentedViewController as! UIAlertController
-        let okAction:UIAlertAction = alert.actions.last!
-        let input:String = (sender.text)!
-        let inputSize = input.characters.count
-        
-        okAction.enabled = (inputSize >= 8 && inputSize <= 16)
-    }
-
-    @IBAction func inputPassword(sender:AnyObject) {
-        let alert = UIAlertController(title: "请输入密码", message: "", preferredStyle: .Alert)
-        let noAction = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
-        let okAction = UIAlertAction(title: "确定", style: .Default,
-            handler: { action in
-                self.m_password = (alert.textFields?.first?.text)!
-                self.m_passBtn.setTitle(self.m_password, forState: .Normal)
-                self.updateNextButton()
-        })
-        alert.addTextFieldWithConfigurationHandler {
-            (textField: UITextField!) -> Void in
-            textField.placeholder = "请输入8到16位密码"
-            textField.keyboardType = .ASCIICapable
-            textField.addTarget(self, action: Selector("passwordChanged:"), forControlEvents: .EditingChanged)
-        }
-        okAction.enabled = false
-        alert.addAction(noAction)
-        alert.addAction(okAction)
-        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     func nickNameChanged(sender:UITextField) {
@@ -319,6 +377,7 @@ class RegView: BaseLogRegView, PhotoClipperDelegate, UINavigationControllerDeleg
         m_countryBtn.layer.cornerRadius = 10
         m_cellBtn.layer.cornerRadius = 10
         m_passBtn.layer.cornerRadius = 10
+        m_codeBtn.layer.cornerRadius = 10
         m_photoBtn.layer.cornerRadius = 10
         m_photoBtn.layer.masksToBounds = true
         m_countryBtn.setTitle(getCountryCodeString(), forState: .Normal)
@@ -326,6 +385,9 @@ class RegView: BaseLogRegView, PhotoClipperDelegate, UINavigationControllerDeleg
         m_picker.m_cliperDelegate = self
         m_enableColor = m_regBtn.backgroundColor
         m_regBtn.backgroundColor = UIColor.grayColor()
+        
+        m_baseCodeBtn = m_codeBtn
+        m_basePassBtn = m_passBtn
     }
     
     func didClippedPickImage(img: UIImage) {
@@ -337,9 +399,10 @@ class RegView: BaseLogRegView, PhotoClipperDelegate, UINavigationControllerDeleg
 
     override func updateNextButton() {
         m_regBtn.enabled =
-            m_cellNumber.characters.count != 0 &&
-            m_password.characters.count != 0 &&
-            m_nickName.characters.count != 0 &&
+            m_cellNumber.characters.count > 0 &&
+            m_password.characters.count > 0 &&
+            m_code.characters.count > 0 &&
+            m_nickName.characters.count > 0 &&
             m_photo != nil
         m_regBtn.backgroundColor = m_regBtn.enabled ? m_enableColor : UIColor.grayColor()
     }
